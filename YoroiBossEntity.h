e24eea5ebfd2023f85954204305e59e8ee063505
Isshin Uno <NK250135@nks.iput.ac.jp>
2026-06-22 21:46:31 +0900

    Yoroiのマージ

--- YoroiBossEntity.h
+++ YoroiBossEntity.h
@@ -14,6 +14,17 @@ protected:
     void UpdateAI(float deltaTime) override;
     void UpdateAttack(float deltaTime) override;
 
+private:
+    // --- 状態管理用変数 ---
+    float m_attackTimer;
+    float m_rollDistanceLeft;
+    int   m_attackStep;
+    int   m_darkAttackCount;
+    int   m_maxDarkAttacks;
 
+    // 弾（斬撃）関連
+    bool     m_bulletActive;
+    Vector2d m_bulletPos;
+    Vector2d m_bulletVel;
 };
 
