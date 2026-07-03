dfd0096ecfff7f7a6c6f547e1c420c6ba786e5a1
Luna-vvzm2 <luna72344929@yahoo.co.jp>
2026-07-03 16:09:48 +0900

    スプライトデータ、stage2のcsv読み込みの更新

--- PlayerEntity.cpp
+++ PlayerEntity.cpp
@@ -45,7 +45,7 @@ PlayerEntity::PlayerEntity(Scene* scene, const Vector2d& pos, const Vector2d& si
     , m_dir(true)
     , m_prevDir(true)
     , m_jumpSpeed(0.0f)
-    , m_moveSpeed(2900.0f)
+    , m_moveSpeed(290.0f)
     , m_dashSpeed(600.0f)
     , m_dashAirSpeed(800.0f)
     , m_dashTimer(0.0f)
@@ -88,7 +88,7 @@ bool PlayerEntity::Init() {
     m_attackCol = AddComponent<CollisionComponent>();
 
     //m_sprite = AddComponent<SpriteComponent>();
-    m_sprite->LoadTextureDiv("assets/images/entities/players/musashi_sheet.png", 20, 12);
+    m_sprite->LoadTextureDiv("assets/images/entities/players/musashi_sheet.png", 20, 13);
     m_sprite->LoadTextureDiv("kari/player.png", 6, 4);
 
     m_anim = AddComponent<AnimationComponent>();
@@ -185,199 +185,223 @@ bool PlayerEntity::Init() {
     m_anim->AddClip("jumpSecond", jumpSecond);
 
     AnimationClip fall;
-    fall.frames = { 54, 55, 56 };
+    fall.frames = { 59, 60, 61 };
     fall.speed = 0.1f;
     fall.loop = false;
     m_anim->AddClip("fall", fall);
 
     AnimationClip jumpLanding;
-    jumpLanding.frames = {57, 58, 59, 60, 61};
+    jumpLanding.frames = { 62, 63, 64, 65, 66 };
     jumpLanding.speed = 0.1f;
     jumpLanding.loop = false;
     m_anim->AddClip("jumpLanding", jumpLanding);
 
     AnimationClip roll;
-    roll.frames = { 62, 63, 64, 65, 66 };
+    roll.frames = { 67, 68, 69, 70, 71 };
     roll.speed = 0.2f;
     roll.loop = false;
     m_anim->AddClip("roll", roll);
 
     AnimationClip Hien;
-    Hien.frames = { 67, 68, 68, 68, 68, 68, 68, 69, 69 };
+    Hien.frames = { 72, 73, 73, 73, 73, 73, 73, 74, 74 };
     Hien.speed = 0.1f;
     Hien.loop = false;
     m_anim->AddClip("Hien", Hien);
 
     AnimationClip Senten;
-    Senten.frames = { 70, 71, 72, 73, 74, 75, 76, 77, 78 };
+    Senten.frames = { 75, 76, 77, 78, 79, 80, 81, 82, 83 };
     Senten.speed = 0.1f;
     Senten.loop = false;
     m_anim->AddClip("Senten", Senten);
 
     AnimationClip rollLanding;
-    rollLanding.frames = { 79, 80, 81 };
+    rollLanding.frames = { 84, 85, 86 };
     rollLanding.speed = 0.1f;
     rollLanding.loop = false;
     m_anim->AddClip("rollLanding", rollLanding);
 
+    AnimationClip SouKunaiSenten;
+    SouKunaiSenten.frames = { 87, 88, 89, 89, 89, 89, 90, 91, 92, 93, 94, 95 };
+    SouKunaiSenten.speed = 0.1f;
+    SouKunaiSenten.loop = false;
+    m_anim->AddClip("SouKunaiSenten", SouKunaiSenten);
+
     AnimationClip wallHold;
-    wallHold.frames = { 82 };
+    wallHold.frames = { 96 };
     wallHold.speed = 0.1f;
     wallHold.loop = false;
     m_anim->AddClip("wallHold", wallHold);
 
     AnimationClip wallJump;
-    wallJump.frames = { 83, 84 };
+    wallJump.frames = { 97, 98 };
     wallJump.speed = 0.1f;
     wallJump.loop = false;
     m_anim->AddClip("wallJump", wallJump);
 
-    AnimationClip wallClimb;
-    wallClimb.frames = { 85, 86, 87, 88 };
-    wallClimb.speed = 0.1f;
-    wallClimb.loop = true;
-    m_anim->AddClip("wallClimb", wallClimb);
-
-    AnimationClip wallClimbUp;
-    wallClimbUp.frames = {89, 89, 90, 91, 91, 92, 93, 94,95, 96, 97, 97 };
+    AnimationClip wallClimbUp; //　壁を歩いて登るアニメーション
+    wallClimbUp.frames = { 99, 100, 101, 102 };
     wallClimbUp.speed = 0.1f;
-    wallClimbUp.loop = false;
+    wallClimbUp.loop = true;
     m_anim->AddClip("wallClimbUp", wallClimbUp);
 
+    AnimationClip wallClimb; // 壁よじ登り
+    wallClimb.frames = { 103, 103, 104, 105, 105, 106 };
+    wallClimb.speed = 0.1f;
+    wallClimb.loop = false;
+    m_anim->AddClip("wallClimb", wallClimb);
+
+    AnimationClip platformClimb;
+    platformClimb.frames = { 107, 108, 108, 109, 110, 110 };
+    platformClimb.speed = 0.1f;
+    platformClimb.loop = false;
+    m_anim->AddClip("platformClimb", platformClimb);
+
+    AnimationClip climbEnd;
+    wallClimb.frames = { 111, 112, 113, 114, 115, 115 };
+    wallClimb.speed = 0.1f;
+    wallClimb.loop = false;
+    m_anim->AddClip("wallClimb", wallClimb);
+
     AnimationClip wallAttack;
-    wallAttack.frames = { 98, 98, 99, 100, 100, 101, 102, 102, 103, 103, 104, 104 };
+    wallAttack.frames = { 116, 116, 117, 118, 118, 119, 120, 120, 121, 121, 122, 122 };
     wallAttack.speed = 0.1f;
     wallAttack.loop = false;
     m_anim->AddClip("wallAttack", wallAttack);
 
+    AnimationClip wallKunai;
+    wallKunai.frames = { 123, 123, 123, 124, 125, 126, 127, 127, 128, 128 };
+    wallKunai.speed = 0.1f;
+    wallKunai.loop = false;
+    m_anim->AddClip("wallKunai", wallKunai);
+
     AnimationClip weakAttack1;
-    weakAttack1.frames = { 105, 106, 106, 107, 107, 107, 107, 107, 107, 107 };
+    weakAttack1.frames = { 129, 130, 130, 131, 131, 131, 131, 131, 131, 131 };
     weakAttack1.speed = 0.1f;
     weakAttack1.loop = false;
     m_anim->AddClip("weakAttack1", weakAttack1);
 
     AnimationClip weakAttack2;
-    weakAttack2.frames = { 108, 109, 110, 110, 111, 111, 112, 113 };
+    weakAttack2.frames = { 132, 133, 134, 134, 135, 135, 136, 137 };
     weakAttack2.speed = 0.1f;
     weakAttack2.loop = false;
     m_anim->AddClip("weakAttack2", weakAttack2);
 
     AnimationClip weakAttack3;
-    weakAttack3.frames = { 114, 115, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116, 116 };
+    weakAttack3.frames = { 138, 139, 140, 140, 140, 140, 140, 140, 140, 140, 140, 140, 140, 140, 140, 140, 140, 140 };
     weakAttack3.speed = 0.1f;
     weakAttack3.loop = false;
     m_anim->AddClip("weakAttack3", weakAttack3);
 
     AnimationClip attackEnd;
-    attackEnd.frames = { 117, 118, 118, 119, 119 };
+    attackEnd.frames = { 141, 142, 142, 143, 143 };
     attackEnd.speed = 0.1f;
     attackEnd.loop = false;
     m_anim->AddClip("attackEnd", attackEnd);
 
     AnimationClip weakAttack4;
-    weakAttack4.frames = { 120, 120, 121, 122, 122, 122, 123, 123, 124, 125, 126, 126, 126, 127, 127, 128, 128, 128, 129, 129, 130, 130, 130, 131, 131, 132 };
+    weakAttack4.frames = { 144, 144, 145, 146, 146, 146, 147, 147, 148, 149, 150, 150, 150, 151, 151, 152, 152, 152, 153, 153, 154, 154, 154, 155, 155, 156 };
     weakAttack4.speed = 0.1f;
     weakAttack4.loop = false;
     m_anim->AddClip("weakAttack4", weakAttack4);
 
     AnimationClip weakAirAttack1;
-    weakAirAttack1.frames = { 133, 134, 135, 135 };
+    weakAirAttack1.frames = { 157, 158, 159, 159 };
     weakAirAttack1.speed = 0.1f;
     weakAirAttack1.loop = false;
     m_anim->AddClip("weakAirAttack1", weakAirAttack1);
 
     AnimationClip weakAirAttack2;
-    weakAirAttack2.frames = { 136, 137, 137, 137, 138 };
+    weakAirAttack2.frames = { 160, 161, 161 };
     weakAirAttack2.speed = 0.1f;
     weakAirAttack2.loop = false;
     m_anim->AddClip("weakAirAttack2", weakAirAttack2);
 
     AnimationClip weakAirAttack3;
-    weakAirAttack3.frames = { 139, 139, 140, 141, 141, 142, 143, 143, 143, 143, 143, 143, 143, 143, 143, 143, 143, 143, 144, 144, 144, 145, 145, 146, 146, 146 };
+    weakAirAttack3.frames = { 162, 162, 163, 164, 164, 165, 166, 166, 166, 166, 166, 166, 166, 166, 166, 166, 166, 166,  167, 167, 167, 168, 168, 169, 169, 169 };
     weakAirAttack3.speed = 0.1f;
     weakAirAttack3.loop = false;
     m_anim->AddClip("weakAirAttack3", weakAirAttack3);
 
     AnimationClip strongAttack1;
-    strongAttack1.frames = { 147, 148, 149, 149, 150, 150, 151, 152, 152, 153, 153, 154, 155, 155, 156, 157, 157 };
+    strongAttack1.frames = { 170, 171, 172, 172, 173, 173, 174, 175, 175, 176, 176, 177, 178, 178, 179 };
     strongAttack1.speed = 0.1f;
     strongAttack1.loop = false;
     m_anim->AddClip("strongAttack1", strongAttack1);
 
     AnimationClip strongAttackEnd;
-    strongAttackEnd.frames = { 157 };
+    strongAttackEnd.frames = { 180, 180 };
     strongAttackEnd.speed = 0.2f;
     strongAttackEnd.loop = false;
     m_anim->AddClip("strongAttackEnd", strongAttackEnd);
 
     AnimationClip strongAttack2;
-    strongAttack2.frames = { 158, 158, 159, 160, 160, 161, 162, 163, 163, 164, 164, 165, 165, 166, 166, 167, 168, 168, 168, 168 };
+    strongAttack2.frames = { 181, 181, 182, 183, 183, 184, 185, 186, 186, 187, 187, 188, 188, 189, 189, 190, 191, 191, 191, 191 };
     strongAttack2.speed = 0.1f;
     strongAttack2.loop = false;
     m_anim->AddClip("strongAttack2", strongAttack2);
 
     AnimationClip Hayabusa;
-    Hayabusa.frames = { 169, 169, 170 };
+    Hayabusa.frames = { 192, 192, 193 };
     Hayabusa.speed = 0.1f;
     Hayabusa.loop = false;
     m_anim->AddClip("Hayabusa", Hayabusa);
 
     AnimationClip HayabusaHit;
-    HayabusaHit.frames = { 171, 171, 171, 172, 172, 173, 173, 174, 174, 175 };
+    HayabusaHit.frames = { 194, 194, 194, 195, 195, 196, 196, 197, 197, 198 };
     HayabusaHit.speed = 0.1f;
     HayabusaHit.loop = false;
     m_anim->AddClip("HayabusaHit", HayabusaHit);
 
     AnimationClip HayabusaGround;
-    HayabusaGround.frames = { 176 };
+    HayabusaGround.frames = { 199 };
     HayabusaGround.speed = 0.1f;
     HayabusaGround.loop = false;
     m_anim->AddClip("HayabusaGround", HayabusaGround);
 
     AnimationClip Kunai;
-    Kunai.frames = { 177, 178, 179, 179, 180, 181 };
+    Kunai.frames = { 200, 201, 202, 202, 203, 204 };
     Kunai.speed = 0.1f;
     Kunai.loop = false;
     m_anim->AddClip("Kunai", Kunai);
 
     AnimationClip KunaiAir;
-    KunaiAir.frames = { 182, 183, 184, 184 };
+    KunaiAir.frames = { 205, 206, 207, 207 };
     KunaiAir.speed = 0.1f;
     KunaiAir.loop = false;
     m_anim->AddClip("KunaiAir", KunaiAir);
 
     AnimationClip KunaiSquat;
-    KunaiSquat.frames = { 185, 186, 186, 187, 187, 188, 188, 189, 190, 190 };
+    KunaiSquat.frames = { 208, 209, 209, 210, 210, 211, 211, 212, 213, 213 };
     KunaiSquat.speed = 0.1f;
     KunaiSquat.loop = false;
     m_anim->AddClip("KunaiSquat", KunaiSquat);
 
     AnimationClip hitTrap;
-    hitTrap.frames = { 191, 192, 193, 194, 195, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196, 196 };
+    hitTrap.frames = { 214, 215, 216, 217, 218, 219, 219, 219, 219, 219, 219, 219, 219, 219, 219, 219, 219, 219, 219, 219, 219 };
     hitTrap.speed = 0.1f;
     hitTrap.loop = false;
     m_anim->AddClip("trapHit", hitTrap);
 
     AnimationClip hitGround;
-    hitGround.frames = { 197, 197, 197, 197, 197, 197, 197, 197, 197, 197, 197, 197, 197, 198, 199, 200, 201, 202 };
+    hitGround.frames = { 220, 220, 220, 220, 220, 220, 220, 220, 220, 220, 220, 220, 220, 221, 222, 223, 224, 225 };
     hitGround.speed = 0.1f;
     hitGround.loop = false;
     m_anim->AddClip("hitGround", hitGround);
 
     AnimationClip hitAir;
-    hitAir.frames = { 203, 203, 203, 203, 203, 203, 203, 203, 203, 203, 203, 203, 203, 204, 204, 205, 205, 206, 206, 207, 208 };
+    hitAir.frames = { 226, 226, 226, 226, 226, 226, 226, 226, 226, 226, 226, 226, 226, 227, 227, 228, 228, 229, 229, 230, 231 };
     hitAir.speed = 0.1f;
     hitAir.loop = false;
     m_anim->AddClip("hitAir", hitAir);
 
     AnimationClip hitAirLanding;
-    hitAirLanding.frames = { 209, 210, 211, 212, 213, 214, 214, 215, 215, 216 };
+    hitAirLanding.frames = { 232, 233, 234, 235, 236, 237, 237, 238, 238, 239 };
     hitAirLanding.speed = 0.1f;
     hitAirLanding.loop = false;
     m_anim->AddClip("hitAir", hitAirLanding);
 
     AnimationClip dead;
-    dead.frames = { 217, 217, 217, 217, 217, 217, 217, 217, 217, 217, 217, 217, 217, 217, 217, 218, 218, 219, 219, 220, 220, 221, 221, 222, 222, 223, 223 };
+    dead.frames = { 240, 240, 240, 240, 240, 240, 240, 240, 240, 240, 240, 240, 240, 240, 240,  241, 241, 242, 242, 243, 243, 244, 244, 245, 245, 246, 246 };
     dead.speed = 0.1f;
     dead.loop = false;
     m_anim->AddClip("dead", dead);
