888022f9e32c3ffea6d0890affaf3d338988785c
Luna-vvzm2 <luna72344929@yahoo.co.jp>
2026-07-09 23:49:31 +0900

    α版最終変更

--- PlayScene.cpp
+++ PlayScene.cpp
@@ -79,7 +79,7 @@ PlayScene::PlayScene(Game* game)
 bool PlayScene::Init() {
 	m_isRunning = true;
 	m_type = Type::Play;
-	m_stageIndex = 2;
+	m_stageIndex = 0;
 	//m_lockedSkillIcon = LoadGraph("assets/images/skills/locked.png");
 	m_menu.Initialize();
 	
